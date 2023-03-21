2b5404e33a374d6d42fe5cbc1684792c736ad618
James Thorniley <james.thorniley@mixcloud.com>
2023-03-21 21:20:13 +0000

    lock only inside cvar

--- asgiref/local.py
+++ asgiref/local.py
@@ -1,56 +1,56 @@
-import threading
 import contextvars
+import threading
 
 class CVar:
     def __init__(self):
-        self._data = contextvars.ContextVar("asgiref-cvar")
-        self._data.set({})
+        self._lock = threading.RLock()
+        with self._lock:
+            self._data = contextvars.ContextVar("asgiref-cvar")
 
     def __getattr__(self, key):
-        storage_object = self._data.get()
-        try:
-            return storage_object[key]
-        except KeyError:
-            raise AttributeError(key)
+        with self._lock:
+            storage_object = self._data.get({})
+            try:
+                return storage_object[key]
+            except KeyError:
+                raise AttributeError(key)
         
     def __setattr__(self, key, value) -> None:
         if key in ("_data", "_lock"):
             return super().__setattr__(key, value)
 
-        storage_object = self._data.get()
-        storage_object[key] = value
-        self._data.set(storage_object)
+        with self._lock:
+            storage_object = self._data.get({})
+            storage_object[key] = value
+            self._data.set(storage_object)
 
     def __delattr__(self, key) -> None:
-        storage_object = self._data.get()
-        del storage_object[key]
-        self._data.set(storage_object)
+        with self._lock:
+            storage_object = self._data.get({})
+            del storage_object[key]
+            self._data.set(storage_object)
         
 
 class Local:
+    """Local storage for async tasks."""
     def __init__(self, thread_critical=False):
-        self._thread_lock = threading.RLock()
-        with self._thread_lock:
-            if thread_critical:
-                # Thread-local storage
-                self._storage = threading.local()
-            else:
-                # Contextvar storage
-                self._storage = CVar()
+        if thread_critical:
+            # Thread-local storage
+            self._storage = threading.local()
+        else:
+            # Contextvar storage
+            self._storage = CVar()
 
     def __getattr__(self, key):
-        with self._thread_lock:
-            return getattr(self._storage, key)
+        return getattr(self._storage, key)
             
     def __setattr__(self, key, value):
-        if key in ("_storage", "_thread_lock"):
+        if key == "_storage":
             return super().__setattr__(key, value)
 
-        with self._thread_lock:
-            setattr(self._storage, key, value)
+        setattr(self._storage, key, value)
 
     def __delattr__(self, key):
-        with self._thread_lock:
-            delattr(self._storage, key)
+        delattr(self._storage, key)
 
         
